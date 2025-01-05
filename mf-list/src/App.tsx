f7d37847f3154cf5ea87b47340ec3856411ae56c
Анна Гловяк <anna.kmeb@gmail.com>
2025-01-05 17:50:15 +0200

    mf-list with react and react router

--- mf-list/src/App.tsx
+++ mf-list/src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
+);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [taskList, setTaskList] = useState([]);
+  useEffect(() => {
+    getTaskList();
+  }, []);
+
+  async function getTaskList() {
+    const { data } = await supabase.from("todos").select();
+    setTaskList(data);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Tasks List</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <ul>
+          {taskList.map((country) => (
+            <li key={country.id}>{country.title}</li>
+          ))}
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="card">
+        <button>Create task</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
