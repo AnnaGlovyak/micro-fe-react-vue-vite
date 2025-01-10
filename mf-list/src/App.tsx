fdb42bac965842193e619c2d5df5e6dd7b8675e8
Анна Гловяк <anna.kmeb@gmail.com>
2025-01-10 20:40:42 +0200

    list mf setup

--- mf-list/src/App.tsx
+++ mf-list/src/App.tsx
@@ -16,6 +16,8 @@ function App() {
 
   async function getTaskList() {
     const { data } = await supabase.from("todos").select();
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
     setTaskList(data);
   }
 
@@ -24,8 +26,8 @@ function App() {
       <h1>Tasks List</h1>
       <div className="card">
         <ul>
-          {taskList.map((country) => (
-            <li key={country.id}>{country.title}</li>
+          {taskList.map((country: any) => (
+            <li key={country.id}>{country?.title}</li>
           ))}
         </ul>
       </div>
